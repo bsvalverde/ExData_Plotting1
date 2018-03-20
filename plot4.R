0fb808524928b46f0130ca607c756d0daaddd32c
bsvalverde <bsvalverde@users.noreply.github.com>
2018-03-19 23:06:21 -0300

    Updated plot 4 and uploaded correct plot1.png

--- plot4.R
+++ plot4.R
@@ -18,6 +18,6 @@ plot(datetime, data$Sub_metering_1, xlab = "", ylab = "Energy sub metering", typ
 points(datetime, data$Sub_metering_1, type = "l")
 points(datetime, data$Sub_metering_2, col = "red", type = "l")
 points(datetime, data$Sub_metering_3, col = "blue", type = "l")
-legend("topright", c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), lty = c(1, 1, 1), col = c("black", "red", "blue"))
+legend("topright", c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), lty = c(1, 1, 1), col = c("black", "red", "blue"), bty = "n")
 with(data, plot(datetime, Global_reactive_power, type = "l"))
 dev.off()
